34181e6b960e8b0936745e5f02ec69f17cb89bdb
Alejandro García <a01716712@tec.mx>
2026-08-25 18:07:35 -0600

    Prueba

---
+++ Hello_Word.py
@@ -0,0 +1,8 @@
+print("Hello World!")                  
+Cal1 = 90
+Cal2 = 100
+Cal3 = 100
+Prom1= (Cal1+Cal2+Cal3)/3  
+Prom2= round(Prom1, 2)
+print("El promedio de las calificaciones es", Prom2, ", la calificacion mas alta  es", max(Cal1, Cal2, Cal3))
+print("y la calificion mas baja es", min(Cal1, Cal2, Cal3))
